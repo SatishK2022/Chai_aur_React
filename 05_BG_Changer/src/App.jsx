77739d8c6cd4e6d0e41e01a0097883b88db8bb3c
SatishK2022 <sk7399052@gmail.com>
2023-08-28 21:21:48 +0530

    Updated button Style

--- 05_BG_Changer/src/App.jsx
+++ 05_BG_Changer/src/App.jsx
@@ -12,84 +12,84 @@ function App() {
         <div className="flex flex-wrap flex-col lg:flex-row gap-5 bg-white px-5 py-3 rounded-lg lg:rounded-full bg-opacity-60 shadow-lg">
           <button
             onClick={() => setColor("red")}
-            className="outline-none px-4 py-1 rounded-full text-white font-semibold"
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
             style={{ backgroundColor: "red" }}
           >
             Red
           </button>
           <button
             onClick={() => setColor("green")}
-            className="outline-none px-4 py-1 rounded-full text-white font-semibold"
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
             style={{ backgroundColor: "green" }}
           >
             Green
           </button>
           <button
             onClick={() => setColor("blue")}
-            className="outline-none px-4 py-1 rounded-full text-white font-semibold"
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
             style={{ backgroundColor: "blue" }}
           >
             Blue
           </button>
           <button
             onClick={() => setColor("yellow")}
-            className="outline-none px-4 py-1 rounded-full font-semibold"
+            className="outline-none px-4 py-1 rounded-full font-semibold shadow-md"
             style={{ backgroundColor: "yellow" }}
           >
             Yellow
           </button>
           <button
             onClick={() => setColor("olive")}
-            className="outline-none px-4 py-1 rounded-full text-white font-semibold"
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
             style={{ backgroundColor: "olive" }}
           >
             Olive
           </button>
           <button
             onClick={() => setColor("grey")}
-            className="outline-none px-4 py-1 rounded-full text-white font-semibold"
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
             style={{ backgroundColor: "grey" }}
           >
             Grey
           </button>
           <button
             onClick={() => setColor("pink")}
-            className="outline-none px-4 py-1 rounded-full font-semibold"
+            className="outline-none px-4 py-1 rounded-full font-semibold shadow-md"
             style={{ backgroundColor: "pink" }}
           >
             Pink
           </button>
           <button
             onClick={() => setColor("purple")}
-            className="outline-none px-4 py-1 rounded-full text-white font-semibold"
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
             style={{ backgroundColor: "purple" }}
           >
             Purple
           </button>
           <button
             onClick={() => setColor("salmon")}
-            className="outline-none px-4 py-1 rounded-full text-white font-semibold"
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
             style={{ backgroundColor: "salmon" }}
           >
             Salmon
           </button>
           <button
             onClick={() => setColor("white")}
-            className="outline-none px-4 py-1 rounded-full font-semibold"
+            className="outline-none px-4 py-1 rounded-full font-semibold shadow-md"
             style={{ backgroundColor: "white" }}
           >
             White
           </button>
           <button
             onClick={() => setColor("skyblue")}
-            className="outline-none px-4 py-1 rounded-full font-semibold"
+            className="outline-none px-4 py-1 rounded-full font-semibold shadow-md"
             style={{ backgroundColor: "skyblue" }}
           >
             Sky Blue
           </button>
           <button
             onClick={() => setColor("teal")}
-            className="outline-none px-4 py-1 rounded-full text-white font-semibold"
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
             style={{ backgroundColor: "teal" }}
           >
             Teal
