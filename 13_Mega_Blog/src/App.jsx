19762c952d308078209ccd35dfe92c9871ed34e3
SatishK2022 <sk7399052@gmail.com>
2023-09-25 13:02:54 +0530

    Setup Environment Variables

---
+++ 13_Mega_Blog/src/App.jsx
@@ -0,0 +1,14 @@
+import config from '../config/config'
+import './App.css'
+
+function App() {
+  console.log(config.appwriteUrl)
+
+  return (
+    <>
+      <h1>A Blog App With Appwrite</h1>
+    </>
+  )
+}
+
+export default App
