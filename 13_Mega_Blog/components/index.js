764bb5615dc57a4b6b371e3edb84a0fcb9bc4bfe
SatishK2022 <sk7399052@gmail.com>
2023-10-13 18:30:20 +0530

    Added Components

--- 13_Mega_Blog/components/index.js
+++ 13_Mega_Blog/components/index.js
@@ -3,6 +3,10 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Container from "./container/Container";
+import Button from './Button'
+import Select from './Select'
+import Postcard from './Postcard'
+import Login from "./Login";
 
 
 export { Header, Footer, Logo, LogoutBtn, Container}
